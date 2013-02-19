5232f1b81ea8e3ac67e3ceeb1dca05fa02f2d2cc
Kyle Jorgensen <kylewjorgensen@gmail.com>
2013-02-19 15:52:03 -0800

    added Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "should have the h1 'Sample App" do
       visit '/static_pages/home'
@@ -10,9 +12,8 @@ describe "StaticPages" do
     
     it "should have the title 'Home" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "| Home")
-      # When searching for the title using 'have_selector' it matches with a substring
-      #   so I didnt need to put "Ruby on Rails Tutorial Sample App | Home"
+      page.should have_selector('title', :text => "#{base_title} | Home")
+      # have selector also matches text to a substring so "| Home" would work
     end
   end
 
@@ -24,7 +25,7 @@ describe "StaticPages" do
     
     it "should have the title 'Help" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "| Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
   
@@ -36,9 +37,21 @@ describe "StaticPages" do
     
     it "should have the title 'About Us" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "| About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
   
+  describe "Contact page" do
+    it "should have the h1 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => "Contact Us")
+    end
+    
+    it "should have the title of 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
+  end
+   
 end
 
