8b6b6e746b19136b2ea497da1f68037979523546
Kyle Jorgensen <kylewjorgensen@gmail.com>
2013-10-20 00:46:06 -0700

    finishes password authentication. Basic User model complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,15 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
 
   before_save { self.email = email.downcase } # to make sure the uniqueness index in the db will work
 
   validates :name, presence: true, length: { maximum: 50 }
+  validates :password, length: { minimum: 6 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true , format: { with: VALID_EMAIL_REGEX }, 
   			uniqueness: { case_sensitive: false }
-
-  has_secure_password
 end
